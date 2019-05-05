6634eaddef28f7a546542b38538591c82a127935
Terry Chau <38119230+TerryChau@users.noreply.github.com>
2019-05-05 08:24:31 -0400

    clear unneeded files

--- app.js
+++ app.js
@@ -1,47 +1,64 @@
-//https://www.geeksforgeeks.org/http-cookies-in-node-js/
-let express = require('express'); 
-let cookieParser = require('cookie-parser'); 
-//setup express app 
-let app = express() 
-  
-app.use(cookieParser()); 
-  
-  
-//basic route for homepage 
-app.get('/', (req, res)=>{ 
-res.send('<h1>Hello, I am the Cookie Monster</h1><form action="/setuser"><input type="submit" formnovalidate value="Go to Google"/></form>'); 
-}); 
-  
-//JSON object to be added to cookie 
-let users = { 
-name : "Ritik", 
-Age : "18"
-} 
-  
-//Route for adding cookie 
-app.get('/setuser', (req, res)=>{ 
-res.cookie("userData", users); 
-res.send('user data added to cookie <h1>Hello, I am the Cookie Monster</h1><form action="/setuser"><input type="submit" value="Go to Google"/></form>'); 
-}); 
-  
-//Iterate users data from cookie 
-app.get('/getuser', (req, res)=>{ 
-//shows all the cookies 
-res.send(req.cookies ); 
-}); 
-  
-//Route for destroying cookie 
-app.get('/logout', (req, res)=>{ 
-//it will clear the userData cookie 
-res.clearCookie('userData'); 
-res.send('user logout successfully <h1>Hello, I am the Cookie Monster</h1><form action="/setuser"><input type="submit" formnovalidate value="Go to Google"/></form>'); 
-}); 
-  
-  
-//server listens to port 3000 
-app.listen(3005, (err)=>{ 
-if(err) res.redirect('/');
-//throw err; 
-console.log('listening on port 3005'); 
-}); 
+/*
+This goal of this exercise is to persist the time at which the stop watch is stopped.  Each stopped time should be listed on a separate line underneath the timer.  For example:
 
+00:00:07
+
+
+Since more than one user may be using this application at once, it is not sufficient to store the list of stopped times in a single array.  Different users would have a different list of stopped times.  Use cookies and sessions to persist the stopped times for difference users.
+
+*/
+
+let express = require('express');
+let app = express();
+
+var fs = require('fs');
+
+
+
+var session=require('express-session');
+app.use(session({
+  secret: 'keyboard cat',
+  resave: 'true', //Forces the session to be saved back to the session store, even if the session was never modified during the request.
+  saveUninitialized: true, //Forces a session that is "uninitialized" to be saved to the store
+  })
+);
+
+app.use(express.urlencoded({extended: false }));
+
+app.get('/', (req, res)=>{
+  var html = fs.readFileSync('./log.html', 'utf8');
+  console.log(req.body.hiddenTime);
+  res.send(html);
+});
+
+app.post('/', (req, res)=>{
+  console.log(req.body.logEntry);
+  console.log(req.body.usrname);
+  console.log(req.body.hiddenTime);
+  console.log("clearform:",req.body.clearform);
+  if(req.body.clearform){
+    req.session.destroy();
+    res.redirect('/');
+  }
+  else{
+  seconds=("0"+(req.body.hiddenTime)%60).slice(-2);
+  minutes=("0"+parseInt((req.body.hiddenTime)/60)).slice(-2);
+  hours=("0"+parseInt((req.body.hiddenTime)/360)).slice(-2);
+  if(!req.session.store){
+    req.session.store="";
+  }
+  req.session.store+="<p>"+hours+":"+minutes+":"+seconds+"</p>"
+  //req.session.store
+  var html = fs.readFileSync('./log.html', 'utf8');
+  console.log(typeof(html));
+  //html=html.replace("<!--Add_Time_Here-->", "<p>cat</p>")
+  html=html.replace("<!--Add_Stopped_Times_Here-->", req.session.store)
+  res.send(html);
+  }
+});
+
+app.listen(3008, (err)=>{ 
+  if(err) //res.redirect('/');
+  throw err; 
+console.log('listening on port 3008'); 
+}); 
