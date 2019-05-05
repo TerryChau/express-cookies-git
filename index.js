a1d1ab103897e420d4fd4b7f20272f79a4e6958e
Terry Chau <38119230+TerryChau@users.noreply.github.com>
2019-05-05 09:58:09 -0400

    problem set is created in index_problem_set.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 /*
-This goal of this exercise is to store the time at which the stop watch is stopped.  Each stopped time should be listed on a separate line underneath the timer in timer.html.  For example, after pressing STOP/RESET 4 times, the html output should be:
+This goal of this exercise is to store the time at which the stop watch is
+stopped.  Each stopped time should be listed on a separate line underneath the
+timer in timer.html.  For example, after pressing STOP/RESET 4 times, the html
+output should be:
 
 00:00:00
 STOP/RESET
@@ -9,9 +12,15 @@ CLEAR
 00:02:05
 00:00:06
 
-Since more than one user may be using this application simultaneously, it is not sufficient to simply store the list of stopped times in a persistent array.  Different users have different lists of stopped times.  Use cookies and/or sessions to persist the stopped times for different users.
+Since more than one user may be using this application simultaneously, it is not
+sufficient to simply store the list of stopped times in a persistent array.
+Different users have different lists of stopped times.  Use cookies and/or
+sessions to persist the stopped times for different users.
 
-Finally, while it is possible to rewrite timer.html in another template engine (Pug, Mustache), it is unnecesary to do so, since 'fs.readFileSync('./timer.html', 'utf8')' produces a string that can be readily edited using Javascript string commands.
+Finally, while it is possible to rewrite timer.html in another template engine, 
+such as Pug or Mustache, it is unnecessary to do so, since
+'fs.readFileSync('./timer.html', 'utf8')' produces a string that can be readily
+edited using Javascript string commands.
 */
 
 var express = require('express');
@@ -22,28 +31,31 @@ var fs = require('fs');
 var session=require('express-session');
 app.use(session({
   secret: 'keyboard cat',
-  resave: 'true', //Forces the session to be saved back to the session store, even if the session was never modified during the request.
-  saveUninitialized: true, //Forces a session that is "uninitialized" to be saved to the store
+  resave: 'true', //forces save, even if session is never modified during request
+  saveUninitialized: true, //forces a session that is "uninitialized" to be saved
   })
 );
 
 //Middleware for parsing incoming requests with urlencoded payloads
 app.use(express.urlencoded({extended: false }));
 
-//Landing Page
+//Landing page
 app.get('/', (req, res)=>{
   var html = fs.readFileSync('./timer.html', 'utf8');
   res.send(html);
 });
 
+//Page after submission of form data
 app.post('/', (req, res)=>{
   if(req.body.clearform){
-    //The user presses 'CLEAR', causing the persistent storage to be erased, and client's browser to be redirected to the landing page.
+    //The user presses 'CLEAR', causing the persistent storage to be erased, and
+    //client's browser to be redirected to the landing page.
     req.session.destroy();
     res.redirect('/');
   }
   else{
-    //The user presses 'STOP/RESET'.
+    //The user presses 'STOP/RESET'.  The time is persistently stored for each
+    //client and displayed in the html.
     seconds=("0"+(req.body.hiddenTime)%60).slice(-2);
     minutes=("0"+parseInt((req.body.hiddenTime)/60)).slice(-2);
     hours=("0"+parseInt((req.body.hiddenTime)/360)).slice(-2);
@@ -57,7 +69,7 @@ app.post('/', (req, res)=>{
   }
 });
 
-app.listen(3000, (err)=>{ 
-  if(err) throw err; 
-  console.log('listening on port 3000'); 
-}); 
+app.listen(3000, (err)=>{
+  if(err) throw err;
+  console.log('listening on port 3000');
+});
